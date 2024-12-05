3114a64cf221416c29b0073e2c29e76a6c7f1e39
Ahmed Raouf <ahmedraouf.metwally@gmail.com>
2024-12-05 17:13:43 +0200

    Register Controller

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,20 @@
+const User = require( '../models/User' )
+const { StatusCodes } = require( 'http-status-codes' )
+const CustomError = require( '../errors/' )
+
 const register = async ( req, res ) =>
 {
-    res.send( "Register" );
+    const { name, email, password } = req.body
+    const emailAlreadyExist = await User.findOne( { email } )
+    if ( emailAlreadyExist )
+    {
+        throw new CustomError.BadRequestError( 'Email already exist' )
+    }
+    // first registered user is an admin
+    const isFirstAccount = await User.countDocuments( {} ) === 0
+    const role = isFirstAccount ? 'admin' : 'user'
+    const user = await User.create( { name, email, password, role } )
+    res.status( StatusCodes.CREATED ).json( { user } )
 }
 
 const login = async ( req, res ) =>
