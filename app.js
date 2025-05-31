195638c95d035dfeb09dafe74a86a6e2257685a5
Ahmed Raouf <ahmedraouf.metwally@gmail.com>
2025-06-01 01:43:59 +0300

    chore: update dependencies to latest versions
    - upgraded @sendgrid/mail from 8.1.4 to 8.1.5
    - upgraded bcryptjs from 2.4.3 to 3.0.2
    - upgraded dotenv from 16.4.7 to 16.5.0
    - upgraded express from 4.21.1 to 5.1.0
    - upgraded express-rate-limit from 7.4.1 to 7.5.0
    - upgraded helmet from 8.0.0 to 8.1.0
    - upgraded mongoose from 8.8.3 to 8.15.1
    - upgraded nodemailer from 6.9.16 to 7.0.3
    - upgraded validator from 13.12.0 to 13.15.15
    - upgraded nodemon from 3.1.7 to 3.1.10

--- app.js
+++ app.js
@@ -1,67 +1,62 @@
 //express
-const express = require( 'express' )
-const app = express()
+const express = require("express");
+const app = express();
 
 // dotenv
-const dotenv = require( 'dotenv' )
-dotenv.config()
+const dotenv = require("dotenv");
+dotenv.config();
 
 // rest of the packages
-const morgan = require( 'morgan' )
-const cookieParser = require( 'cookie-parser' )
+const morgan = require("morgan");
+const cookieParser = require("cookie-parser");
 // async errors
-require( 'express-async-errors' )
+// require("express-async-errors"); // Uncomment this line if you want to use express-async-errors for error handling
 
 //database
-const connectDB = require( './db/connect' )
+const connectDB = require("./db/connect");
 
 //routers
-const authRouter = require( './routes/authRoutes' )
+const authRouter = require("./routes/authRoutes");
 
 // middleware
-const notFoundMiddleware = require( './middleware/not-found' )
-const errorHandlerMiddleware = require( './middleware/error-handler' )
+const notFoundMiddleware = require("./middleware/not-found");
+const errorHandlerMiddleware = require("./middleware/error-handler");
 
 // morgan
-app.use( morgan( 'tiny' ) )
+app.use(morgan("tiny"));
 
 // middleware to parse json
-app.use( express.json() )
+app.use(express.json());
 // middleware to parse cookies
-app.use( cookieParser(process.env.JWT_SECRET))
+app.use(cookieParser(process.env.JWT_SECRET));
 
 // home route
-app.get( '/', ( req, res ) =>
-{
-    
-    res.send( 'E-Commerce API' )
-} )
-app.get( '/api/v1', ( req, res ) =>
-{
-    console.log( req.signedCookies );
+app.get("/", (req, res) => {
+  res.send("E-Commerce API");
+});
+app.get("/api/v1", (req, res) => {
+  console.log(req.signedCookies);
 
-    res.send( 'E-Commerce API' )
-} )
+  res.send("E-Commerce API");
+});
 // routes
-app.use( '/api/v1/auth', authRouter )
+app.use("/api/v1/auth", authRouter);
 
 // middleware to handle errors
-app.use( notFoundMiddleware ) // middleware to handle not found routes
-app.use( errorHandlerMiddleware ) // middleware to handle errors
+app.use(notFoundMiddleware); // middleware to handle not found routes
+app.use(errorHandlerMiddleware); // middleware to handle errors
 
-const port = process.env.PORT || 3000 // port to listen on
+const port = process.env.PORT || 3000; // port to listen on
 
 // start
-const start = async () =>
-{
-    try
-    {
-        await connectDB( process.env.MONGO_URL )
-        await app.listen( port, console.log( `Server started on port ${ port }...` ) )
-    } catch ( error )
-    {
-        console.error( error )
-        process.exit( 1 )
-    }
-}
-start() // start the server
+const start = async () => {
+  try {
+    // connect to the database
+    await connectDB(process.env.MONGO_URL);
+    await app.listen(port, console.log(`Server started on port ${port}...`));
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+};
+start(); // start the server
